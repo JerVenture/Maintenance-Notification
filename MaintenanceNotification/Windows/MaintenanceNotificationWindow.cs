5867845e7b4d8e6f1bb47fdfe6659dea3999788a
JerVenture <jerventure89@gmail.com>
2026-03-22 19:08:40 -0400

    Added a flashing window to the main window, where it flashes between red and black every half a second and stops after 10 seconds. The title also alternates in time with the flashing.

--- MaintenanceNotification/Windows/MaintenanceNotificationWindow.cs
+++ MaintenanceNotification/Windows/MaintenanceNotificationWindow.cs
@@ -13,6 +13,9 @@ public class MaintenanceNotificationWindow : Window
 
     private IFontAtlas fontAtlas;
     private IFontHandle maintenanceFontHandle;
+    private bool titleFlash = false;
+    private float flashTimer = 0f;
+    private float flashElapsed = 0f;
 
     public MaintenanceNotificationWindow(IFramework framework) : base("MAINTENANCE NOTIFICATION")
     {
@@ -32,14 +35,55 @@ public class MaintenanceNotificationWindow : Window
             var viewportSize = ImGui.GetMainViewport().Size;
             Size = new Vector2(viewportSize.X * 0.6f, viewportSize.Y * 0.6f);
             Position = ImGui.GetMainViewport().GetCenter() - ((Size ?? new Vector2(800, 500)) / 2);
+
+            if (flashElapsed < 10f)
+            {
+                if (titleFlash) 
+                    ImGui.PushStyleColor(ImGuiCol.WindowBg, new Vector4(0.3f, 0f, 0f, 1f));
+                else
+                    ImGui.PushStyleColor(ImGuiCol.WindowBg, new Vector4(0f, 0f, 0f, 1f));
+            }
+
         }  
     
     public override void Draw()
     {
         using (maintenanceFontHandle.Push())
         {
-                    ImGui.TextWrapped(MaintenanceMessage);
+            ImGui.TextWrapped(MaintenanceMessage);
         }
+    }
+
+    public override void PostDraw()
+    {
+        if (flashElapsed < 10f)
+            ImGui.PopStyleColor();
+    }
+
+    public override void Update()
+    {
+        if (flashElapsed >= 10f)
+        {
+            WindowName = "MAINTENANCE NOTIFICATION";
+            return;
+        }
+
+        flashElapsed += ImGui.GetIO().DeltaTime;
+        flashTimer += ImGui.GetIO().DeltaTime;
+
+        if (flashTimer >= 0.5f)
+        {
+            flashTimer = 0f;
+            titleFlash = !titleFlash;
+            WindowName = titleFlash ? "MAINTENANCE NOTIFICATION" : "***MAINTENANCE SOON***";
+        }
+
+    }
+
+    public override void OnOpen()
+    {
+        flashElapsed = 0f;
+        flashTimer = 0f;
     }
 
     public void dispose()
